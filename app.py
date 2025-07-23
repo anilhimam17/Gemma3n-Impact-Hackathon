71729334687095dc1a3e81c6a737fd5cec5d73ad
anilhimam17 <anilhimamg@gmail.com>
2025-07-24 00:03:30 +0100

    feat: MVP UI achievedgit add .git add .git add .git add .

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 import gradio as gr
 from gradio_pdf import PDF
+from src.query_engine import QueryEngine
+from pathlib import Path
+import json
 
 
 class GradioInterface:
     """Implements the complete interface for a page in Gradio."""
     def __init__(self):
+
+        # Gradio Layout and Component Instance Variables
         self.block_params = {"title": "Research Companion", "fill_height": True, "fill_width": True}
         self.mark_style = 'background-color: #c9c8c7; padding: 0.2em 0.4em; border-radius: 5px;'
         self.desciption_style = 'text-align: center; line-height: 2.5; font-size: 16px;'
@@ -19,33 +24,93 @@ class GradioInterface:
         </div>
         """
 
+        # Query Engine Parameters
+        self.query_engine = None
+
+
     # ==== Interface Builder ====
     def page(self) -> None:
         """Implements a complete Gradio Interface using the Components."""
 
         # The main block enclosing the entire interface.
         with gr.Blocks(**self.block_params) as demo:
-            gr.Markdown(value="<h1 style='text-align: center; font-size: 32px;'>Research Companion 🤖</h1>")
-            gr.HTML(value=self.top_description)
+
+            # Sidebar for additional features
+            with gr.Sidebar(position="left"):
+                user_name = gr.Markdown(value="### Hi Username")
+
+            # Top Division for the Header
+            with gr.Row():
+                # Header
+                _ = gr.Markdown(value="<h1 style='text-align: center; font-size: 32px;'>Research Companion 🤖</h1>")
+            with gr.Row():
+                # Description
+                _ = gr.HTML(value=self.top_description)
 
             # Splitting the page into two sections
             with gr.Row():
                 with gr.Column(scale=1):
-                    pdf_comp = PDF(label="Upload your PDF")
-                with gr.Column(scale=2):
-                    gr.ChatInterface(
-                        fn=self.run_query, type="messages",
-                        chatbot=gr.Chatbot(placeholder="Let's begin the Research", type="messages"),
-                        additional_inputs=[pdf_comp, gr.Audio(label="Voice Input", sources=["microphone"], type="numpy")]
+                    pdf_comp = PDF(label="Upload PDF", interactive=True)
+                with gr.Column(scale=1):
+                    chatbot = gr.Chatbot(
+                        label="Research Companion", bubble_full_width=False, 
+                        placeholder="Upload a PDF and begin the Research Journey."
+                    )
+                    message_box = gr.Textbox(label="Ask questions about the Document", interactive=True)
+                    voice_input = gr.Audio(label="Speak with the Document", interactive=False)
+                    submit_button = gr.Button(value="Submit")
+
+                    gr.on(
+                        triggers=[message_box.submit, submit_button.click],
+                        fn=self.run_query,
+                        inputs=[pdf_comp, message_box, chatbot],
+                        outputs=[chatbot, message_box]
                     )
         
         # Rendering the page.
         demo.launch()
 
     # ==== Helper Functions ====
-    @staticmethod
-    def run_query(message, pdf_input = None, audio = None, state: None = None) -> str:
+    def run_query(self, pdf_path: str, message: str, history: list):
         """Propagates the given query through the AI agent."""
-        last_message = message if message else ""
-        # audio_status = "Audio Recieved." if audio is not None else "No Audio."
-        return f"User Said: {last_message}"
+        
+        # If no message was sent
+        if not message:
+            yield history, ""
+            return
+
+        # If pdf was not uploaded
+        if not pdf_path:
+            history.append((message, "Please upload a PDF document first."))
+            yield history, "" 
+            return
+        
+        # Begin Thinking Process
+        history.append((message, "Thinking..."))
+        yield history, ""
+
+        # If the query engine was not created yet or the PDF has changed
+        if self.query_engine is None or self.query_engine.file_path != Path(pdf_path):
+            self.query_engine = QueryEngine(pdf_path)
+
+        # Generating a response
+        response_json = self.query_engine.run_query(message)
+        response_data = json.loads(response_json)
+        answer = response_data.get("answer", "Sorry no answer was found")
+
+        citations = response_data.get("citations", [])
+        citations_markdown = ""
+        if citations:
+            citations_markdown = "\n\n---\n\n**Sources & Citations**\n\n"
+            for idx, citation in enumerate(response_data.get("citations")):
+                source_text = citation.get('source_text', 'N/A').replace('\n', ' ')
+                citations_markdown += ( 
+                    f"**{idx + 1}. Source from Page {citation.get('page_number', 'N/A')}:**\n" 
+                    f"> {source_text}\n\n" 
+                    f"*Simplified Explanation:*\n{citation.get('simplification', 'N/A')}\n\n" 
+                )
+        
+        final_response = f"{answer}{citations_markdown}"
+        history[-1] = (message, final_response)
+        yield history, ""
+        return
--- src/query_engine.py
+++ src/query_engine.py
@@ -10,9 +10,9 @@ from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from llama_index.core.query_engine import BaseQueryEngine
 from llama_index.core.response_synthesizers import ResponseMode
 
-from config import settings
-from response_structures import ResearchResponse
-from structured_prompt import CUSTOM_PROMPT_TEMPLATE
+from src.config import settings
+from src.response_structures import ResearchResponse
+from src.structured_prompt import CUSTOM_PROMPT_TEMPLATE
 
 from pathlib import Path
 from typing import cast
@@ -25,38 +25,37 @@ class QueryEngine:
     It creates a vector index for the input files and constructs a Query Engine (soon extended to Chat Engine).
     The Query Engine encapsulates the end - to - end workflow executing the RAG pipeline with Gemma3n:e4b model."""
 
-    def __init__(self, filename: str = "YOLOv7.pdf") -> None:
+    def __init__(self, filepath: str = "assets/YOLOv7.pdf") -> None:
         """Class Constructor."""
         self.llm = Ollama(model=settings.llm_model_name, request_timeout=300.0, context_window=4000)
         self.structured_llm = self.llm.as_structured_llm(ResearchResponse)
 
         self.documents: list[Document] = []
-        self.vector_store: VectorStoreIndex  # Current VectorStore only create Indexes using Text, need to extend to MultiModal => Todo.
+        self.vector_store: VectorStoreIndex  
         self.embed_model = HuggingFaceEmbedding(model_name=settings.embedding_model_name) 
 
-        self.file_name: Path = Path(filename)
-        self.file_registry: Path = settings.asset_path
         self.index_registry: Path = settings.vector_store_path
-        self.file_path: Path = self.file_registry / filename
-
+        self.file_path: Path = Path(filepath)
         self.query_engine = self.construct_query_engine()
 
     def check_index_exists(self) -> bool:
         """Checks if the index for a given file already exists."""
-        return (self.index_registry / self.file_name.stem).exists()
+        return (self.index_registry / self.file_path.stem).exists()
 
     def construct_query_engine(self) -> BaseQueryEngine:
         """Loads, Transforms and Indexes the input file / reloads them if exits and provides a query engine object."""
+        index_dir = self.index_registry / self.file_path.stem
+
         if not self.check_index_exists():
-            # Creating the Document
-            self.documents = SimpleDirectoryReader(self.file_registry).load_data()
+            # Creating the Document from the specific file path
+            self.documents = SimpleDirectoryReader(input_files=[self.file_path]).load_data()
             # Calculating the Indexes
             self.vector_store = VectorStoreIndex.from_documents(self.documents, embed_model=self.embed_model, show_progress=True)
             # Storing the Indexes for reuse
-            self.vector_store.storage_context.persist(persist_dir=self.index_registry / self.file_name.stem)
+            self.vector_store.storage_context.persist(persist_dir=index_dir)
         else:
             # Loading the Indexes
-            storage_context = StorageContext.from_defaults(persist_dir=str(self.index_registry / self.file_name.stem))
+            storage_context = StorageContext.from_defaults(persist_dir=str(index_dir))
             intermediate_index = load_index_from_storage(storage_context=storage_context, embed_model=self.embed_model)
             self.vector_store = cast(VectorStoreIndex, intermediate_index)
 
@@ -83,8 +82,10 @@ class QueryEngine:
         return research_response_output.model_dump_json(indent=4)
 
 
-gemma_engine = QueryEngine()
-for i in range(3):
-    inp_prompt = input("Enter Query: ")
-    response = gemma_engine.run_query(inp_prompt)
-    print(response)
+# ==== Unit Test ====
+if __name__ == "__main__":
+    gemma_engine = QueryEngine()
+    for i in range(3):
+        inp_prompt = input("Enter Query: ")
+        response = gemma_engine.run_query(inp_prompt)
+        print(response)
